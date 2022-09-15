f9c17acd85d6a81f79dc03508444cb4261781535
Ali Ibne masud <mdmasudrony@gmail.com>
2022-09-15 15:40:02 +0600

    design modified

--- src/componants/Pages/Dashboard/StudentsRow.jsx
+++ src/componants/Pages/Dashboard/StudentsRow.jsx
@@ -4,36 +4,36 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 
-const StudentsRow = ({student, index, setStudentInfo, setUpadateInfo}) => {
-    const {name, fatherName, motherName, class:clsssNo, _id} = student;
+const StudentsRow = ({ student, index, setStudentInfo, setUpadateInfo }) => {
+    const { name, fatherName, motherName, class: clsssNo, _id } = student;
 
     const handleDelete = id => {
         const confrimDelete = window.confirm(`Are you sure want to delete ${name}`);
-        
-        if(confrimDelete){
-           axios.delete(`https://guarded-spire-40443.herokuapp.com/student/${id}`)
-           .then(res => {
-            toast.success(`${name} profile has been deleted.`)
-           })
+
+        if (confrimDelete) {
+            axios.delete(`https://guarded-spire-40443.herokuapp.com/student/${id}`)
+                .then(res => {
+                    toast.success(`${name} profile has been deleted.`)
+                })
         }
-    }  
+    }
 
     return (
         <tr>
-            <th>{index+1}</th>
+            <th>{index + 1}</th>
             <td>{name}</td>
             <td>{clsssNo}</td>
             <td>{fatherName}</td>
             <td>{motherName}</td>
-            <td>
-                <label htmlFor='update-student-info' onClick={()=> setUpadateInfo(student)}>
-                    <PencilSquareIcon className="h-6 w-6 text-blue-500" />
+            <td className='flex'>
+                <label title='View' htmlFor='view-student-info' onClick={() => setStudentInfo(student)}>
+                    <EyeIcon className="h-6 w-6 text-green-500 cursor-pointer" />
                 </label>
-                <label onClick={()=> handleDelete (_id)} className=' mx-3'>
-                    <TrashIcon className="h-6 w-6 text-red-600 " />
+                <label title='Edit' className='mx-3' htmlFor='update-student-info' onClick={() => setUpadateInfo(student)}>
+                    <PencilSquareIcon className="h-6 w-6 text-blue-500 cursor-pointer" />
                 </label>
-                <label htmlFor='view-student-info' onClick={()=> setStudentInfo(student)}>
-                    <EyeIcon className="h-6 w-6 text-green-500" />
+                <label title='Delete' onClick={() => handleDelete(_id)} >
+                    <TrashIcon className="h-6 w-6 text-red-600 cursor-pointer" />
                 </label>
             </td>
         </tr>
